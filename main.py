a3a5400bee00076d354e5f5d7e1da4be680e2a76
Rohan <117934079+rohanhreddy@users.noreply.github.com>
2023-01-22 15:49:55 +0530

    .gitignore & first draft

---
+++ main.py
@@ -0,0 +1,40 @@
+#calculator operations as functions
+
+def add():
+    print("placeholder")
+
+def sub():
+    print("placeholder")
+
+def mult():
+    print("placeholder")
+
+def div():
+    print("placeholder")
+
+def floor_div():
+    print("placeholder")
+
+def mod():
+    print("placeholder")
+
+def exp():
+    print("placeholder")
+
+def abs():
+    print("placeholder")
+
+def lim():
+    print("placeholder")
+
+def dif():
+    print("placeholder")
+
+def inte():
+    print("placeholder")
+
+#inputs for operation
+
+
+
+#run operation
