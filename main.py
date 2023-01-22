080b930778345cb4b8c718c0984d3abc975e4eac
Rohan <117934079+rohanhreddy@users.noreply.github.com>
2023-01-22 17:03:30 +0530

    basic build test
    no major updates to code, testing exe build with imported modules

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+#importing
+
+from sympy import symbols, limit
+
 #calculator operations as functions
 
-def add():
-    print("placeholder")
+def add(x = 1, y = 1):
+    return (x+y)
 
-def sub():
-    print("placeholder")
+def sub(x = 1, y = 1):
+    return (x-y)
 
-def mult():
-    print("placeholder")
+def mult(x = 1, y = 1):
+    return (x*y)
 
-def div():
-    print("placeholder")
+def div(x = 1, y = 1):
+    return (x/y)
 
-def floor_div():
-    print("placeholder")
+def floor_div(x = 1, y = 1):
+    return (x//y)
 
-def mod():
-    print("placeholder")
+def mod(x = 1, y = 1):
+    return (x%y)
 
-def exp():
-    print("placeholder")
+def exp(x = 1, y = 1):
+    return (x**y)
 
-def abs():
-    print("placeholder")
+def abs(x):
+    if (x < 0):
+        x = x * -1
+        return (x)
+    else:
+        return(x)
 
 def lim():
     print("placeholder")
@@ -35,6 +43,18 @@ def inte():
 
 #inputs for operation
 
+print("\nWelcome to a 'Simple' Calculator!\nThis Calculator is completely made in Python and supports 11 different operations!\n")
 
+print("0 - Addition")
+print("1 - Subtraction")
+print("2 - Multiplication")
+print("3 - Division (Returns Quotient)")
+print("4 - Floor Division (Returns only the Integral Part of the Quotient)")
+print("5 - Modulus (Returns Remainder)")
+print("6 - Exponent (Performs an Exponential Operation on two numbers)")
+print("7 - Absolute Value (Returns Magnitude)")
+print("8 - Limits (Performs a Limit Operation)")
+print("9 - Differentiation (Performs a Differentiation Operation on a single term)")
+print("10 - Integration (Performs an Integration Operation on a single term)\n")
 
-#run operation
+o = int(input("hold"))
